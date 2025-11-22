906577f4d079d0bdc768478d3d6441ec933ed1e0
italotes <italotiago.es@outlook.com>
2025-11-22 18:17:23 +0000

    5 - Calculando Média de Notas

---
+++ 5 - Calculando Média de Notas.py
@@ -0,0 +1,14 @@
+# Solicita ao usuário que digite a primeira nota e converte para float
+nota1 = float(input("Digite a primeira nota: "))
+
+# Solicita ao usuário que digite a segunda nota e converte para float
+nota2 = float(input("Digite a segunda nota: "))
+
+# Solicita ao usuário que digite a terceira nota e converte para float
+nota3 = float(input("Digite a terceira nota: "))
+
+# Calcula a média das três notas usando o operador aritmético de soma e divisão
+media = (nota1 + nota2 + nota3) / 3
+
+# Exibe o resultado da média
+print("A média das notas é:", media)
