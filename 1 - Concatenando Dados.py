025054540234af7774513d1f9bfda083c65e1790
italotes <italotiago.es@outlook.com>
2025-11-22 17:21:25 +0000

    Código da questão 1

---
+++ 1 - Concatenando Dados.py
@@ -0,0 +1,10 @@
+
+# Recebe dois dados do usuário
+dado1 = input(" Digite o primeiro dado: ")
+dado2 = input(" Digite o segundo dado: ")
+
+# Concatena os dados em uma única string
+resultado = dado1 + dado2
+
+# Exibe o resultado
+print("O resultado da concatenação é:", resultado)
